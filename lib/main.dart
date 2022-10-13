1147d4cd07fe08221251bccecf90f7164b6cece0
Kupeli <omerkupeliq@gmail.com>
2022-10-13 18:39:19 +0300

    buildNavIvon adlı widget ayrı dosya olarak yazıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+
 import 'package:mobil_bilet1/core/widgets/buildEventNav.dart';
 import 'package:mobil_bilet1/core/widgets/buildFeatured.dart';
-
 import 'package:mobil_bilet1/core/widgets/button.dart';
 import 'package:mobil_bilet1/core/widgets/buildBaslik.dart';
+import 'package:mobil_bilet1/core/widgets/buildNavIcon.dart';
 
 void main() {
   runApp(const MyApp());
@@ -278,19 +279,4 @@ class MyApp extends StatelessWidget {
           ),
         ));
   }
-}
-
-Widget buildNavIcon(@required IconData iconData, @required bool active) {
-  return ClipRRect(
-    borderRadius: BorderRadius.all(Radius.circular(12)),
-    child: Container(
-      color: active ? Colors.blue : Color.fromARGB(166, 201, 201, 201),
-      padding: EdgeInsets.all(8),
-      child: Icon(
-        iconData,
-        size: 25,
-        color: Colors.white,
-      ),
-    ),
-  );
 }
