b866e5e5562d97efa1ab6945377313b101160349
omerkupeli <omerkupeliq@gmail.com>
2023-04-19 14:17:36 +0300

    pub update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:mobil_bilet1/screens/home.dart';
 import 'package:mobil_bilet1/screens/loginPage.dart';
 import 'package:mobil_bilet1/screens/profilePage.dart';
 import 'package:mobil_bilet1/screens/registerPage.dart';
+import 'package:mobil_bilet1/screens/ticket.dart';
 
 import 'Event.dart';
 import 'MyNotifications.dart';
@@ -24,45 +25,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ProfilePage(),
+      home: HomePage(),
     );
   }
 }
-
-Event event = Event(
-    name: "Tarkan Konseri",
-    category: "Müzik",
-    location: "Altıeylül, Balıkesir",
-    date: "17 Aralık, 2023",
-    imageUrl: "https://cdn.bubilet.com.tr/files/Blog/resim-adi-76524.png",
-    going: "20K+ Gidiyor...",
-    time: "20:00 - 23:00",
-    venue: "Altıeylül, Balıkesir Spor Salonu",
-    images: [
-      "https://picsum.photos/seed/980/600",
-      "https://picsum.photos/seed/980/600",
-      "https://picsum.photos/seed/980/600"
-    ],
-    description:
-        "Bu örnekte, SingleChildScrollView ile sarmalanmış bir Column widget'ı var. ScrollController örneği oluşturuldu ve SingleChildScrollView'a atandı. Hedef bölge, yeşil renkteki bir Container ile temsil edilir ve _targetOffset sabiti ile belirlenir. Düğme, _scrollToTarget yöntemi çağrılarak tıklanır ve ScrollController'ın animateTo yöntemi kullanılarak sayfa hedef bölgeye kaydırılır.");
-
-Event event2 = Event(
-    name: "Haluk Levent Konseri",
-    category: "Müzik",
-    location: "Bandırma, Balıkesir",
-    date: "24 Mart 2023",
-    imageUrl:
-        "https://iasbh.tmgrup.com.tr/611b76/650/344/0/49/800/470?u=https://isbh.tmgrup.com.tr/sbh/2021/09/01/balikesirde-konser-veren-haluk-leventten-uzen-haber-haluk-levent-koronaviruse-yakalandigini-acikladi-1630516590439.jpg",
-    going: "25K+ Gidiyor...",
-    venue: "Bandırma, Bandrma Spor Salonu",
-    time: "20:00 - 23:00",
-    images: [
-      "https://picsum.photos/seed/980/600",
-      "https://picsum.photos/seed/980/600",
-      "https://picsum.photos/seed/980/600"
-    ],
-    description:
-        "Bu örnekte, SingleChildScrollView ile sarmalanmış bir Column widget'ı var. ScrollController örneği oluşturuldu ve SingleChildScrollView'a atandı. Hedef bölge, yeşil renkteki bir Container ile temsil edilir ve _targetOffset sabiti ile belirlenir. Düğme, _scrollToTarget yöntemi çağrılarak tıklanır ve ScrollController'ın animateTo yöntemi kullanılarak sayfa hedef bölgeye kaydırılır.");
 
 MyNotificaitons myNotificaitons = MyNotificaitons(
     title: "Tarkan Konseri",
--- lib/screens/createEvent.dart
+++ lib/screens/createEvent.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 
 class CreateEvent extends StatefulWidget {
